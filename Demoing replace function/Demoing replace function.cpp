e84c31c84c0fa3e3a9db960e60e27b7846be3428
Work <Work@MSI>
2024-10-31 10:22:37 -0500

    Where we ended on Oct 31 - resume with anagram checking

--- Demoing replace function/Demoing replace function.cpp
+++ Demoing replace function/Demoing replace function.cpp
@@ -2,16 +2,51 @@
 //
 
 #include <iostream>
-
+#include<cassert>
 #include<string> 
 
 using std::string; 
 using std::cout; 
 
+void ourOwnReplaceFunction(string& inputString, int indexAtWhichToReplace, char theCharacterToPutIn)
+{
+	//string newString; 
+	assert(indexAtWhichToReplace < inputString.length()); //prevents going out of bounds
+	assert(indexAtWhichToReplace >= 0); 
+
+	inputString[indexAtWhichToReplace] = theCharacterToPutIn; 
+
+	//return newString;
+
+}
+
+/*READ THIS documentation so that you don't misuse this function...*/
+void ourOwnReplaceFunction(string& inputString, int startPosition, int endPosition, char theCharacterToPutIn)
+{
+	for (int i = startPosition; i <= endPosition; i++) {
+		ourOwnReplaceFunction(inputString, i - 1, theCharacterToPutIn); //credit: Mr. Ball (jummy_juce) 
+	}
+}
 
 int main()
 {
-	string someWord = "Alice"; 
 
-	string lowerCaseSomeWord = someWord.replace('A', 'a') //...to be continued 
+	string someWord = "Alice";
+
+	//ourOwnReplaceFunction(someWord, 0, 'a');
+
+	//cout << "Have we replaced A with a? " << "\n";
+	//cout << someWord << "\n";
+
+	//replace the text "Alice" with "A****"? 
+	//ourOwnReplaceFunction(someWord, 1, someWord.length(), '*');
+
+	//someWord.replace(0, 2, "Bob"); //Ali -> Bobce
+
+	cout << someWord << "\n";
+	//someWord.replace( //Soundex algorithm 
+	//someWord.replace()
+	//string lowerCaseSomeWord = someWord.replace('A', 'a') //...to be continued 
+	
+	
 }
---
+++ Playing with anagrams/Playing with anagrams.cpp
@@ -0,0 +1,59 @@
+// Playing with anagrams.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include<fstream> 
+#include <string>
+
+#include<algorithm>
+
+using std::cout; 
+using std::string; 
+using std::ifstream; 
+
+int main()
+{
+	cout << "Enter a word - we'll look for ANAGRAMS of it ...\n";
+	string inputWord; 
+	std::cin >> inputWord;  //ex:coronavirus 
+
+	//string anAnagramOfInputWord = "carnivorous"; 
+
+	//cout << "Sorted version of input string " << inputWord << "is: ";
+	//std::sort(inputWord.begin(), inputWord.end());
+	//cout << inputWord << "\n";
+
+	//cout << "Sorted version of anagram " << anAnagramOfInputWord << "is : ";
+	//std::sort(anAnagramOfInputWord.begin(), anAnagramOfInputWord.end()); 
+	//cout << anAnagramOfInputWord << "\n";
+
+
+	//system("pause");
+
+	ifstream dictionaryFile{ "C:/Users/Work/Downloads/popular.txt" }; //HERE!
+
+	if (!dictionaryFile.is_open())
+	{
+		cout << "FNFE\n";
+		return 1;
+	}
+
+	std::sort(inputWord.begin(), inputWord.end());
+
+	while (!dictionaryFile.eof())
+	{
+		string currentWord; 
+		getline(dictionaryFile, currentWord);
+
+		string copyOfCurrentWord = currentWord; 
+
+		std::sort(currentWord.begin(), currentWord.end());
+
+		if (inputWord == currentWord)
+		{
+			cout << 
+		}
+		//cout << currentWord << "\n";
+	}
+
+}
